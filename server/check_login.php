292c5b9bd27938b943d3659de512da4f6d67b874
harlinacero <harlinacero@gmail.com>
2018-02-21 16:22:51 -0500

    codigo base

---
+++ server/check_login.php
@@ -0,0 +1,23 @@
+<?php
+  require('./conector.php');
+
+  $con = new ConectorBD('localhost','root','');
+
+  $response['conexion'] = $con->initConexion('transporte_db');
+
+  if ($response['conexion']=='OK') {
+    $resultado_consulta = $con->consultar(['usuarios'],
+    ['email', 'psw'], 'WHERE email="'.$_POST['username'].'" AND psw="'.$_POST['passw'].'"');
+
+    if ($resultado_consulta->num_rows != 0) {
+      $response['acceso'] = 'concedido';
+    }else $response['acceso'] = 'rechazado';
+  }
+
+  echo json_encode($response);
+
+  $con->cerrarConexion();
+
+
+
+ ?>
